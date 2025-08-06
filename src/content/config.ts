9dce64dc71aaa9016ef5c64732cc879b02623783
Xabier Figueoa <xabier.figueroa96@gmail.com>
2025-08-06 21:48:38 +0200

    feat: update RoomCard and Showcase components to use new room data structure; enhance room JSON configurations with additional properties

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,23 @@
 import { defineCollection, z } from "astro:content";
 
-const gamesCollection = defineCollection({
-  type: "content",
-  schema: z.object({
+const rooms = defineCollection({
+  type: "data",
+  schema: ({ image }) => z.object({
+    id: z.string(),
     title: z.string(),
     slug: z.string(),
-    image_full: z.string(),
-    image_thumbnail: z.string(),
+    image: image(),
+    image_thumbnail: image(),
     hook: z.string(),
     theme: z.array(z.string()),
     difficulty: z.string(),
+    players: z.string(),
+    time: z.string(),
+    description: z.string(),
+    color: z.string().optional(),
   }),
 });
 
 export const collections = {
-  games: gamesCollection,
+  rooms,
 };
